beba474d5d5924292f93cfa6c3ceb0335111496a
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2021-08-30 18:19:21 +0200

    Update style.js

--- style.js
+++ style.js
@@ -16,6 +16,14 @@
 
 				{ name: 'Título 1', element: 'h4', attributes: { 'class': 'bck-title bck-title1'} },
 				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
+				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title bck-title3'} },
+				{ name: 'Título 4', element: 'h4', attributes: { 'class': 'bck-title bck-title4'} },
+				{ name: 'Título 5', element: 'h4', attributes: { 'class': 'bck-title bck-title5'} },
+				{ name: 'Título 6', element: 'h4', attributes: { 'class': 'bck-title bck-title6'} },
+				{ name: 'Título 7', element: 'h4', attributes: { 'class': 'bck-title bck-title7'} },
+				{ name: 'Título 8', element: 'h4', attributes: { 'class': 'bck-title bck-title8'} },
+				{ name: 'Título 9', element: 'h4', attributes: { 'class': 'bck-title bck-title9'} },
+
 
 				{ name: 'Lista ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
 				{ name: 'Lista ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
