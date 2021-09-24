a53fda44294a4da41d5013a177ee2a2b3642eff7
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2021-09-24 14:50:15 +0200

    Update style.js

--- style.js
+++ style.js
@@ -29,6 +29,7 @@
 				{ name: 'Título 13', element: 'h4', attributes: { 'class': 'bck-title bck-title13'} },
 				{ name: 'Título 14', element: 'h4', attributes: { 'class': 'bck-title bck-title14'} },
 				{ name: 'Título 15', element: 'h4', attributes: { 'class': 'bck-title bck-title15'} },
+				{ name: 'Título 16', element: 'h4', attributes: { 'class': 'bck-title bck-title16'} },
 
 				{ name: 'Énfasis 1', element: 'span', attributes: { 'class': 'bck-enfasis-1'} },
 				{ name: 'Énfasis 2', element: 'span', attributes: { 'class': 'bck-enfasis-2'} },
